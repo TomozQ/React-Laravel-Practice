dd19121b3d2e22ed22bff30874877e1026daff6c
TomozQ <inf.4clover@gmail.com>
2021-01-16 15:26:50 +0900

    create header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,6 +2,7 @@
 <header class="header">
     <div class='header-container'>
         <h1 class='header-title'>Apparel Inventory Control</h1>
+        <img src="images/logo.png" alt="LOGO" class='header-logo'>
     </div>
 </header>
 @endsection
